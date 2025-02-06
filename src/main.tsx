2a29e849c261dac4cc888c928ae5512b8c34bd55
royts05 <tilberoyette02@gmail.com>
2025-02-06 23:01:41 +0800

    first commit

--- src/main.tsx
+++ src/main.tsx
@@ -6,7 +6,6 @@ import { ThemeProvider } from '@emotion/react';
 import { CssBaseline } from '@mui/material';
 import { theme } from 'theme/theme.ts';
 import './index.css';
-// import App from './App';
 import { UserProvider } from './contexts/UserContext';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
